471c6d84073c83f0915a8655dd44c0652af6e992
greenfructose <justinclarkturney@gmail.com>
2022-03-23 09:11:30 -0500

    Won't builf for some reason

--- app/hello_django/wsgi.py
+++ app/hello_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for justinturneycom project.
+WSGI config for hello_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'justinturneycom.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_django.settings')
 
 application = get_wsgi_application()
